4482691628c74472fc2a9e3cf274930bdc7975a7
iosAlbuDavid <david.albu04@e-uvt.ro>
2025-04-06 22:33:52 +0300

    Add files via upload
    Adaugare formular login si register.

--- LibraryPlus/settings.py
+++ LibraryPlus/settings.py
@@ -62,7 +62,7 @@ INTERNAL_IPS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- LibraryPlus/urls.py
+++ LibraryPlus/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
+    path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
