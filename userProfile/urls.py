f11b5914b83586174a411a443af31167d9e61f26
LucaTarniceriu <165795845+LucaTarniceriu@users.noreply.github.com>
2025-05-05 23:40:23 +0300

    saved books continuare

--- userProfile/urls.py
+++ userProfile/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('', views.profile_view, name='profile'),
     path('saved-books/', views.saved_books_view, name='saved_books'),
     path('wishlist/', views.wishlist_view, name='wishlist'),
     path('my-ratings/', views.my_ratings_view, name='my_ratings'),
+
 ]
+
